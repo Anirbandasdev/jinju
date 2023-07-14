f389a0168000be0e188d09ebdb19afd879a9d47d
Anirban Das <Anirbandas892@gmail.com>
2023-07-14 19:19:11 +0530

    nav sticky

---
+++ index.js
@@ -0,0 +1,22 @@
+// navbar sticky
+
+const section_hero = document. querySelector( "#page1");
+const Header = document. querySelector( ".header" );
+
+const observer = new IntersectionObserver((entries) => {
+    const ent = entries[0];
+    console.log(ent);
+    ent.isIntersecting === false // for header sticky scrolling treger
+    ? Header.classList.add("sticky")
+    : Header.classList.remove("sticky");
+    ent.isIntersecting === false // for hero-section mirgin scrolling treger
+    ? section_hero.classList.add("stickyhero")
+    : section_hero.classList.remove("stickyhero");
+
+}, {
+    root: null,
+    rootMargin: "-200px",
+    threshold: 0,
+});
+
+observer.observe(section_hero);
